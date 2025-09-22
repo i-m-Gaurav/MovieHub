7ec8375901a6f2cedcece77dd6bfb13e87066358
Gaurav <indiagauravkumar@gmail.com>
2025-09-22 13:23:46 +0530

    done

---
+++ backend/README.md
@@ -0,0 +1,2 @@
+# mboard
+# mboard
---
+++ backend/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import cors from "cors";
+import connect from "./src/database/db.js";
+import dotenv from "dotenv";
+import userRoutes from './src/routes/userRoutes.js';
+import movieRoutes from './src/routes/movieRoutes.js';
+
+
+
+
+const app = express();
+dotenv.config();
+
+
+connect();
+app.use(express.json());
+app.use(cors());
+
+
+
+
+app.use('/api/user',userRoutes);
+app.use('/api/movies',movieRoutes);
+
+app.use("/", (req, res) => {
+    res.send("API is running....");
+});
+
+
+
+// app.listen(3000);
+
+
+export default app;
